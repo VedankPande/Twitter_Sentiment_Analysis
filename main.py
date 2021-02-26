0d435ee82910f293fdc07934c8b8a420bae2976e
VedankPande <vedank.pande@gmail.com>
2021-02-27 00:57:06 +0530

    commits

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ import treelib
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+
+from sklearn import preprocessing
 
 
 if __name__ == '__main__':
 
     # argument parser
-    parser = argparse.ArgumentParser(description='Twitter Sentiment analysis using the Twitter API')
+    parser = argparse.ArgumentParser(description='Twitter Sentiment analysis')
 
     #keyword argument for tweet search
-    parser.add_argument('keyword',type=str,
+    parser.add_argument('--keyword',type=str,
                         help='keyword used to query the twitter search-enter either a valid twitter handle of keyword(s)')
     # number of tweets to search/retrieve, default = 10
     parser.add_argument('--num_tweets',type=int,
@@ -26,11 +29,24 @@ if __name__ == '__main__':
     arguments = parser.parse_args()
 
     # Driver code
-    cursor = search_user_status(arguments.keyword,arguments.num_tweets)
-    tree = get_user_tweet_replies(cursor)
+    #retrieve and save new data
 
-    tree = analyze_data(tree)
-    save_data(tree,"manutd")
-    plot_df(get_dataframe(tree))
+    #cursor = search_user_status(arguments.keyword,arguments.num_tweets)
+    #tree = get_user_tweet_replies(cursor)
+    #tree = analyze_data(tree)
+    #save_data(tree,"manutd")
+
+    #load data (during dev)
+    tree = load_data("manutd.pickle")
+
+    df = get_dataframe(tree)
+    #normalize sentiment values in dataframe
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x = np.reshape(df['sentiment'].values,(-1,1))
+    x = min_max_scaler.fit_transform(x)
+    df['sentiment'] = np.reshape(x,(-1,))
+
+    #plot dataframe
+    plot_df(df)
 
 
--- plot.py
+++ plot.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from sklearn import preprocessing
+
 
 #TODO: Fix code and add dates on x axis
 def plot_df(df):
+    #preprocess data
+    #min_max_scaler = preprocessing.MinMaxScaler()
+    #df['sentiment'] = min_max_scaler.fit_transform(df['sentiment'])
+
     #init plot
     fig,ax = plt.subplots()
 
